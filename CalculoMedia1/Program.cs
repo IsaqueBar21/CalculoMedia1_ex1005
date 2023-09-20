e656214f5af07a85072144532124a0827f53842f
Isaque Barbosa da Silva <isaque_barbosa@jabil.com>
2023-09-20 07:00:01 -0300

    primeiro commit

---
+++ CalculoMedia1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+class URI
+{
+    static void Main(string[] args)
+    {
+
+        double A, B, C;
+
+        A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        C = ((A * 3.5) + (B * 7.5)) / 11;
+
+        Console.WriteLine("MEDIA = " + C.ToString("f5", CultureInfo.InvariantCulture));
+
+    }
+
+}
